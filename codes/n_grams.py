7f5e9b4a7e598d9483d8044eb59f138f82d1fd9e
mikeyy <adhikari.shrish1@gmail.com>
2022-08-31 22:14:56 +0545

    Removed countries

--- codes/n_grams.py
+++ codes/n_grams.py
@@ -3,17 +3,22 @@
 import nltk
 from nltk.corpus import stopwords
 
+
 import sys
 
 nltk.download('stopwords', download_dir="../data/nltk_data/")
 nltk.data.path = ["../data/nltk_data/"]
 
+
 def generate_N_grams(text, ngram=1):
-    words = [word for word in text.split(" ") if word not in set(stopwords.words('english'))]
+    words = [word.strip() for word in text.split(" ") if word not in set(stopwords.words('english')) and "-" not in word]
     temp = zip(*[words[i:] for i in range(ngram)])
     ans = [' '.join(ngram) for ngram in temp]
     return ans
 
+
+print(generate_N_grams("North Carolina Department is very corruput 2-1-1", 3))
+print(generate_N_grams("service  Department pokemon", 3))
 # def check_ngrams(l, ngram):
 #     # print(l, ngram)
 #     # print(l[0].split(" "))
