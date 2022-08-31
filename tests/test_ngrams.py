e1644fa8866fd43cc71e50cf12047f52c5b73911
mikeyy <adhikari.shrish1@gmail.com>
2022-08-31 14:27:55 +0545

    Simple Testing done with assert

--- tests/test_ngrams.py
+++ tests/test_ngrams.py
@@ -1,21 +1,26 @@
 #!/bin/python3
 import sys
 
-# print(sys.argv)
-fi = sys.argv[1]
-n = int(sys.argv[2])
 
-with open(f"data/ngram/{fi}.txt", "r") as f:
-    d = [data.strip() for data  in f.readlines()]
     
-def check_ngrams(l, ngram):
+def test_ngrams(l, ngram):
     # print(l, ngram)
     # print(l[0].split(" "))
-    return all([True if len(x.split()) == ngram else False for x in l])
+    for i in l:
+        assert len(i.split()) == ngram, "Should be Equal"
+    # return all([True if len(x.split()) == ngram else False for x in l])
 
 
-if check_ngrams(d, n):
-    print(f"{fi}.txt contents are {n}-gram")
-else:
-    print(f"{fi}.txt contents aren't {n}-gram")
 
+
+if __name__ == '__main__':
+    
+    # print(sys.argv)
+    fi = sys.argv[1]
+    n = int(sys.argv[2])
+
+    with open(f"data/ngram/{fi}.txt", "r") as f:
+        d = [data.strip() for data  in f.readlines()]
+    
+    test_ngrams(d, n)
+    print("Test Passed!")
